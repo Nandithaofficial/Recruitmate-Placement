7618b2b6c23badd56570865d05493f0f14f4a05f
Nanditha <nanditha.kallu2023@gmail.com>
2026-03-30 20:50:38 +0530

    fix cors duplication

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import 'dotenv/config'; 
 import express from "express";
 import cors from "cors";
-//import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authroutes.js";
 import resumeRoutes from "./routes/resumeRoutes.js";
@@ -11,21 +10,19 @@ import aiRoutes from "./routes/aiRoutes.js";
 import studyMaterialRoutes from "./routes/studyMaterialRoutes.js";
 import progressRoutes from "./routes/progressRoutes.js";
 
-
-//dotenv.config();
 connectDB();
 
 const app = express();
-const cors = require("cors");
+
 app.use(cors());
-app.use("/uploads", express.static("uploads"));
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
 app.use("/api/resume", resumeRoutes);
 app.use("/api/ats", atsRoutes);
 app.use("/api/ai", aiRoutes);
 app.use("/api/materials", studyMaterialRoutes);
 app.use("/api/progress", progressRoutes);
-
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
